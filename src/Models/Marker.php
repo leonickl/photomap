1242bdb08a197f3911f6cc4e4941381a86830451
Leo Nickl <leo@leonickl.de>
2026-04-21 08:55:13 +0200

    skip empty coordinates

--- src/Models/Marker.php
+++ src/Models/Marker.php
@@ -30,6 +30,10 @@ class Marker extends Model
         $GPSLatitude = $exif['GPS']['GPSLatitude'];
         $GPSLongitudeRef= $exif['GPS']['GPSLongitudeRef'];
         $GPSLongitude = $exif['GPS']['GPSLongitude'];
+
+        if ($GPSLatitudeRef === null || $GPSLongitudeRef === null) {
+            return null;
+        }
         
         $lat_degrees = count($GPSLatitude) > 0 ? self::gpsToNum($GPSLatitude[0]) : 0;
         $lat_minutes = count($GPSLatitude) > 1 ? self::gpsToNum($GPSLatitude[1]) : 0;
@@ -39,8 +43,8 @@ class Marker extends Model
         $lon_minutes = count($GPSLongitude) > 1 ? self::gpsToNum($GPSLongitude[1]) : 0;
         $lon_seconds = count($GPSLongitude) > 2 ? self::gpsToNum($GPSLongitude[2]) : 0;
         
-        $lat_direction = ($GPSLatitudeRef == 'W' or $GPSLatitudeRef == 'S') ? -1 : 1;
-        $lon_direction = ($GPSLongitudeRef == 'W' or $GPSLongitudeRef == 'S') ? -1 : 1;
+        $lat_direction = ($GPSLatitudeRef == 'W' || $GPSLatitudeRef == 'S') ? -1 : 1;
+        $lon_direction = ($GPSLongitudeRef == 'W' || $GPSLongitudeRef == 'S') ? -1 : 1;
         
         $latitude = $lat_direction * ($lat_degrees + ($lat_minutes / 60) + ($lat_seconds / (60*60)));
         $longitude = $lon_direction * ($lon_degrees + ($lon_minutes / 60) + ($lon_seconds / (60*60)));
