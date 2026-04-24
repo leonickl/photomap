0496bf18655bf21cb410df260d4e5fda6b16057e
Julian Harrer <julian.harrer@posteo.de>
2026-04-24 18:33:28 +0200

    Add in-app camera with GPS location capture
    - New camera view for taking photos directly in the browser
    - Automatic GPS coordinate capture when photo is taken
    - Manual coordinate fallback if GPS unavailable
    - Camera button on map for quick access
    - Refactored MapController with shared validation and save methods
    - Supports rear camera on mobile devices

--- routes.php
+++ routes.php
@@ -8,6 +8,8 @@ use PXP\Router\Route;
 Route::get('/')->do(MapController::class, 'index');
 Route::get('/create')->do(MapController::class, 'create');
 Route::post('/')->do(MapController::class, 'store');
+Route::get('/camera')->do(MapController::class, 'camera');
+Route::post('/camera')->do(MapController::class, 'storeFromCamera');
 
 Route::get('/css/{file}')->do(AssetController::class, 'css');
 Route::get('/img/{file}')->do(ImageController::class, 'img');
